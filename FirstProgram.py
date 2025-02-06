41bf5587da482f24d970b6b5cafd84acce58c2e6
Kamlesh Yadav <kamleshdagar1998@gmail.com>
2025-02-06 14:20:19 +0530

    First Project

---
+++ FirstProgram.py
@@ -0,0 +1,23 @@
+# a = int("2")
+# b = 4.5
+
+# print(type(a))
+# print(a + b)
+
+# a = int(input("Enter First Value: "))
+# b = int(input("Enter Second Value: "))
+
+# print("Sum = ", a + b)
+
+marks = int(input("Enter student marks :"))
+
+if(marks >= 90):
+    grade = "A"
+elif(marks >= 80 and marks < 90):
+    grade = "B"
+elif(marks >= 70 and marks < 80):
+    grade = "C"
+else:
+    grade = "D"
+
+print("Grade is :",grade)
